650f4d00531b12ac59414b573c1b66c787d26f02
Ege Hurturk <ege.hurturk@gmail.com>
2022-12-02 08:14:25 +0300

    backend

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,10 @@ from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+from dalle import *
+
+import os
+
 
 # engine = create_engine("postgresql://egehurturk:egehurturk@localhost:5432/sessiontest")
 # db = scoped_session(sessionmaker(bind=engine))
@@ -17,3 +21,7 @@ Session(app)
 def reset():
     return render_template("../frontend/html/ndex.html")
 
+@app.route("/api", method=["POST"])
+def api():
+    download_image_from(extract_image_url(dalle.generate_image(request.form.get("prompt")))
+    return render_template("../frontend/html/result.html")
